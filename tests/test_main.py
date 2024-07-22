b4277881486968976f778f95a8e8932d9d01c660
Fatima Jamali <135330185+fatimajamali81@users.noreply.github.com>
2024-07-22 16:35:25 +0500

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+# tests/test_main.py
+
+import pytest
+from app.main import add, subtract
+
+def test_add():
+    assert add(3, 5) == 8
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(0, 5) == -5
+
+if __name__ == "__main__":
+    pytest.main()
